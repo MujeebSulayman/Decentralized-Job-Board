ddc5c8df8432cc599642afc46307f5dc6abbdf82
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2025-06-06 10:08:31 +0100

    frontend

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@
 
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import JobBoard from "@/components/JobBoard";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <Header />
       <Hero />
-      <JobBoard />
     </div>
   );
 }
